d0bab8584df26288d17477f3acf10d93ede1a46e
unknown <stanislav.jarysta@codeacademylt.onmicrosoft.com>
2021-12-26 13:49:41 +0200

    13.49

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -3,10 +3,13 @@ import Product from "./Product";
 
 const Shopping = ({products, buy, changeWindow}) => {
     return (
+
+
         <div className="d-flex f-wrap grow1">
             {products.map(x => <Product buyProduct={buy} item={x} key={x.id}/>)}
 
-        <div className="btn" onClick={() => changeWindow(3)}>Shop</div>
+        <div className="btn" onClick={() => changeWindow(1)}>Home</div>
+            <div className="btn" onClick={() => changeWindow(3)}>Product</div>
     </div>
 
     );
